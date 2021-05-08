6ca63ebaf0d9c77e6cda3a7fe041417dd05ba134
phrj94 <ph.rj.94@hotmail.com>
2021-05-08 09:43:35 -0300

    Adicionando Styled-Components ao projeto

--- src/components/Country.js
+++ src/components/Country.js
@@ -3,9 +3,90 @@ import Data from './Data.js';
 import Columns from './Columns.js';
 import { useSortBy, useTable } from 'react-table';
 import { FaCaretSquareUp, FaCaretSquareDown } from "react-icons/fa";
-import "./Country.css";
+//import "./Country.css";
+import styled from 'styled-components';
+
+//Styled components
+const TableStyled = styled.table`
+    border-spacing: 0;
+    border-collapse: collapse;
+    border: 1px solid black;
+    position: relative;
+`;
+
+const ThStyled = styled.th`
+    margin: 0;
+    padding: 0.5rem;
+    border-bottom: 1px solid gray;
+    border-right: 1px solid gray;
+    text-align: center;
+    width: 70px;
+    height: 22px;
+    background-color: black;
+    color: white;
+
+    :last-child{
+        border-right: 0;
+    }
+
+    div + span{
+        width: 80%;
+        float: left;
+    }
+    
+`;
+
+const TdStyled = styled.td`
+    margin: 0;
+    padding: 0.5rem;
+    border-bottom: 1px solid black;
+    border-right: 1px solid black;
+
+    :last-child{
+        border-right: 0;
+    }
+`;
+
+const TrStyled = styled.tr`
+    :nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    last-child td {
+        border-bottom: 0;
+    }
+
+    > th > div > span {
+        position: relative;
+        float: right;
+    }
+`;
+
+const SpanTitleHeaderStyled = styled.span`
+    width: 80%;
+    float: left;
+`;
+
+const SpanSortIconStyled = styled.span`
+    width: 20%;
+`;
+
+const DivContainer = styled.div`
+    padding-top: 25px;
+    display: flex;
+    justify-content: center;
+
+    @media only screen and (max-width: 768px){
+        padding-top: 0;
+        display: flex;
+        overflow-x: auto;
+        justify-content: left;
+    }
+`
+
 
 function Country(props) {
+    
     const data = Data(props.countries);
     const columns = Columns();
 
@@ -18,22 +99,22 @@ function Country(props) {
     } = useTable({ columns, data }, useSortBy);
 
     return (
-        <div className="container">
-            <table {...getTableProps()}>
-                <thead >
+        <DivContainer>
+            <TableStyled {...getTableProps()}>
+                <thead>
                     {headerGroups.map(headerGroup => (
                         <tr {...headerGroup.getHeaderGroupProps()} >
                             {headerGroup.headers.map(column => (
-                                <th className="t-head" {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                <ThStyled {...column.getHeaderProps(column.getSortByToggleProps())}>
                                     <div>
-                                        <span className="titleHeader">
+                                        <SpanTitleHeaderStyled>
                                             {column.render("Header")}
-                                        </span>
-                                        <span className="sort">
+                                        </SpanTitleHeaderStyled>
+                                        <SpanSortIconStyled >
                                             {column.isSorted ? (column.isSortedDesc ? (<FaCaretSquareDown /> ) : ( <FaCaretSquareUp /> ) ) : ("")}
-                                        </span>
+                                        </SpanSortIconStyled>
                                     </div>
-                                </th>
+                                </ThStyled>
                             ))}
                         </tr>
                     ))}
@@ -42,20 +123,20 @@ function Country(props) {
                     {rows.map(row => {
                         prepareRow(row);
                         return (
-                            <tr {...row.getRowProps()}>
+                            <TrStyled {...row.getRowProps()}>
                                 {row.cells.map(cell => {
                                     return (
-                                        <td {...cell.getCellProps()}>
+                                        <TdStyled {...cell.getCellProps()}>
                                             { cell.render("Cell")}
-                                        </td>
+                                        </TdStyled>
                                     );
                                 })}
-                            </tr>
+                            </TrStyled>
                         );
                     })}
                 </tbody>
-            </table>
-        </div>
+            </TableStyled>
+        </DivContainer>
     )
 }
 
